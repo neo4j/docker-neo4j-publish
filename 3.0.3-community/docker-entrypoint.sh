41f1ce7f6df13d0b42d9c44cbc925c14876e8595
Mohammad Khodadadi <MkhIr@users.noreply.github.com>
2016-06-26 16:56:12 +0100

    Separate arg for initial heap size

--- 3.0.3-community/docker-entrypoint.sh
+++ 3.0.3-community/docker-entrypoint.sh
@@ -20,7 +20,7 @@ if [ "$1" == "neo4j" ]; then
     setting "dbms.tx_log.rotation.retention_policy" "${NEO4J_dbms_txLog_rotation_retentionPolicy:-100M size}" neo4j.properties
     setting "dbms.memory.pagecache.size" "${NEO4J_dbms_memory_pagecache_size:-512M}" neo4j.properties
     setting "wrapper.java.additional=-Dneo4j.ext.udc.source" "${NEO4J_UDC_SOURCE:-docker}" neo4j-wrapper.conf
-    setting "dbms.memory.heap.initial_size" "${NEO4J_dbms_memory_heap_maxSize:-512}" neo4j-wrapper.conf
+    setting "dbms.memory.heap.initial_size" "${NEO4J_dbms_memory_initial_maxSize:-512}" neo4j-wrapper.conf
     setting "dbms.memory.heap.max_size" "${NEO4J_dbms_memory_heap_maxSize:-512}" neo4j-wrapper.conf
     setting "dbms.unmanaged_extension_classes" "${NEO4J_dbms_unmanagedExtensionClasses:-}" neo4j-server.properties
     setting "dbms.allow_format_migration" "${NEO4J_dbms_allowFormatMigration:-}" neo4j.properties
